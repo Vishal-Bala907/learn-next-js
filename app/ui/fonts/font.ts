cfe693cf5c7d26646855c902b5b1a209c12eba0c
vishal bala linux <vishal.bala.100@gmail.com>
2025-06-07 19:00:14 +0530

    initial commit

---
+++ app/ui/fonts/font.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, IBM_Plex_Sans } from "next/font/google";
+// import { IBM_Plex_Sans } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], weight: "400" });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+// import { Inter } from "next/font/google";
+
+// export const inter = Inter({ subsets: ["latin"] });
